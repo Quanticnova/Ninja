df71e44d2b4b93e96e5bd95436ed08666d9c4b60
Siddarth SIngh <infester67@gmail.com>
2018-07-25 22:31:24 +0200

    Added Timers
    Added attack Timer
    Added hit detection script

--- Ninja Cheaters/Assets/Scripts/AttackScript.cs
+++ Ninja Cheaters/Assets/Scripts/AttackScript.cs
@@ -6,7 +6,11 @@ public class AttackScript : MonoBehaviour {
 
     int scalingFramesLeft = 0;
     bool isAttacking = false;
-    bool isShrinking = false;
+    bool canAttack = true;
+    public int player = 0;
+
+    public float TimerBuffer = 2f;
+    public float Timer = 0;
 
 
     public Vector3 targetAngle = new Vector3(0f, 180, 0f);
@@ -20,13 +24,24 @@ public class AttackScript : MonoBehaviour {
 
     public void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S) )
+
+        
+
+        if(Input.GetKeyDown(KeyCode.S) && canAttack)
         {
             targetAngle = new Vector3(0f, transform.rotation.x + 180, 0f);
             isAttacking = true;
             scalingFramesLeft = 40;
+            canAttack = false;
+             Timer = Time.time + TimerBuffer;
             
         }
+
+        if(Time.time > Timer)
+        {
+            canAttack = true;
+        }
+
 
         if(isAttacking && scalingFramesLeft > 0)
         {
@@ -47,5 +62,13 @@ public class AttackScript : MonoBehaviour {
         }
       
 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerHit>().PlayerNum != player)
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
