26aa99b6932e693d2af0eca0b070930fabc7712a
TiffanyMiller <41308741+TiffanyMiller@users.noreply.github.com>
2018-07-30 20:14:02 +0200

    Movement and Input changes

---
+++ Ninja Cheaters/Assets/Scenes/Scenes/Invisible.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invisible : MonoBehaviour {
+
+    public float fadeSpeed = 0.1f;
+    private float spawnTime;
+    private Material[] enemyMaterials;
+    public bool collided;
+
+    private ScoreManager TheScoreManager;
+
+    // Use this for initialization
+    void Start ()
+    {
+        TheScoreManager = FindObjectOfType<ScoreManager>();
+
+        collided = false;
+
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        enemyMaterials = GetComponent<MeshRenderer>().materials;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (collided == true)
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+            SetAlpha((Time.time - spawnTime) * fadeSpeed);            
+        }
+    }
+
+    void SetAlpha(float alpha)
+    {
+        // Change the alpha value of each materials' color
+        for (int i = 0; i < enemyMaterials.Length; ++i)
+        {
+            Color color = enemyMaterials[i].color;
+            color.a = Mathf.Clamp(alpha, 0, 1);
+            enemyMaterials[i].color = color;
+        }
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "P1Weapon")
+        {
+            print(collided);
+            collided = true;
+            TheScoreManager.P1ScoreCount += 1; 
+        }
+
+        if (col.gameObject.tag == "P2Weapon")
+        {
+            collided = true;
+            TheScoreManager.P2ScoreCount += 1;
+        }
+
+        if (col.gameObject.tag == "P3Weapon")
+        {
+            collided = true;
+            TheScoreManager.P3ScoreCount += 1;
+        }
+
+        if (col.gameObject.tag == "P4Weapon")
+        {
+            collided = true;
+            TheScoreManager.P4ScoreCount += 1;
+        }
+    }
+}
---
+++ Ninja Cheaters/Assets/Scripts/PlayerJoin.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerJoin : MonoBehaviour {
+
+    private GameObject currentButton;
+    private AxisEventData currentAxis;
+
+    private float timeBetweenInputs = 0.15f, timer = 0;
+
+    private void Update()
+    {
+        if(timer == 0)
+        {
+            currentAxis = new AxisEventData(EventSystem.current);
+            currentButton = EventSystem.current.currentSelectedGameObject;
+
+            if (Input.GetAxis("HorizontalJoy") > 0)     // move right
+            {
+                currentAxis.moveDir = MoveDirection.Right;
+                ExecuteEvents.Execute(currentButton, currentAxis, ExecuteEvents.moveHandler);
+                timer = timeBetweenInputs;
+            }
+            else if (Input.GetAxis("HorizontalJoy") < 0)    // move left
+            {
+                currentAxis.moveDir = MoveDirection.Left;
+                ExecuteEvents.Execute(currentButton, currentAxis, ExecuteEvents.moveHandler);
+                timer = timeBetweenInputs;
+            }
+        }
+
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+        }
+    }
+}
